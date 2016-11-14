2480f92006481ec79eb61ac11253afab580631ff
budwaiser4 <budwaiser4@yahoo.it>
2016-11-14 12:05:35 +0100

    Header completato

--- functions.php
+++ functions.php
@@ -103,10 +103,10 @@ add_action( 'widgets_init', 'traveldude_widgets_init' );
  */
 function traveldude_scripts() {
 	wp_enqueue_style( 'traveldude-style', get_stylesheet_uri() );
-	wp_enqueue_style( 'main', get_template_directory_uri() . '/layouts/bootstrap.css' );
+	wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/layouts/bootstrap.css' );
 	wp_enqueue_style( 'main', get_template_directory_uri() . '/layouts/main.css' );
 	wp_enqueue_script( 'traveldude-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-	wp_enqueue_script( 'traveldude-navigation', get_template_directory_uri() . '/layouts/bootstrap.min.js', array(), '20151215', true );
+	wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/layouts/bootstrap.min.js', array(), '20151215', true );
 	wp_enqueue_script( 'traveldude-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
--- header.php
+++ header.php
@@ -20,30 +20,8 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'traveldude' ); ?></a>
-
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
-
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'traveldude' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
-
-	<div id="content" class="site-content">
+<div class="header">
+	<div class="logo text-center">
+		<a href="<?php echo get_home_url(); ?>"><img src="<?php echo get_template_directory_uri() . '/img/Traveldude_logo.png'?>"  width="212px" height="43px"></a>
+	</div>
+</div>
