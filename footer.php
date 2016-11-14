ce360423b5d86bbc9cf7101ab423bfe82e230a6c
budwaiser4 <budwaiser4@yahoo.it>
2016-11-14 12:06:30 +0100

    Reset footer

--- footer.php
+++ footer.php
@@ -11,17 +11,6 @@
 
 ?>
 
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'traveldude' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'traveldude' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'traveldude' ), 'traveldude', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
-
 <?php wp_footer(); ?>
 
 </body>
